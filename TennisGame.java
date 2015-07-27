00eeb6ac0552c362205c9242e85494c75cbf7768
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-27 11:04:24 +0530

    computeScores function added

--- TennisGame.java
+++ TennisGame.java
@@ -12,6 +12,32 @@ class TennisGame
 		playerA = 0;
 		playerB = 0;
 	}
+	
+	void computeScores(String scores)
+    {
+        boolean wins = false;
+        for (int i = 0; i < scores.length(); i++) {
+            if (scores.charAt(i) == 'A') {
+                playerA++;
+               
+            }
+            
+            else
+            
+            if (scores.charAt(i) == 'B') {
+                playerB++;   
+            }
+            
+            wins = checkIfWon();
+                if(wins) {
+                    playerA = 0;
+                    playerB = 0;
+                }
+                else
+                    printScores();      
+        }
+    }
+    
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner scanner = new Scanner(System.in);
