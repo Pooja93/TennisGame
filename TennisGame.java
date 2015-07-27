807bd8657bc1e77b87c04eda775b0aa163aba7d5
unknown <test@L-MAA-25004240.paypalcorp.com>
2015-07-27 11:19:55 +0530

    Improving code aesthetics

--- TennisGame.java
+++ TennisGame.java
@@ -7,66 +7,53 @@ class TennisGame
 	int playerA;
 	int playerB;
 	
-	TennisGame()
-	{
+	TennisGame() {
 		playerA = 0;
 		playerB = 0;
 	}
 	
-	void computeScores(String scores)
-    {
+	void computeScores(String scores) {
         boolean wins = false;
         for (int i = 0; i < scores.length(); i++) {
             if (scores.charAt(i) == 'A') {
                 playerA++;
-               
-            }
-            
-            else
-            
-            if (scores.charAt(i) == 'B') {
+            } else if (scores.charAt(i) == 'B') {
                 playerB++;   
             }
-            
             wins = checkIfWon();
                 if(wins) {
                     playerA = 0;
                     playerB = 0;
-                }
-                else
+                } else
                     printScores();      
         }
     }
 	
-	boolean checkIfWon()
-    {
+	private void printScores() {
+		System.out.println("printing scores");
+	}
+
+	boolean checkIfWon() {
         boolean wins = false;
         
-        if (playerA >= 3 && (playerA - playerB == 2))
-        {
+        if (playerA >= 3 && (playerA - playerB == 2)){
             System.out.println("Player A wins");
             wins = true;
-        }
-        else
-        if (playerB >=3 && (playerB - playerA == 2))
-        {
+        } else if (playerB >=3 && (playerB - playerA == 2)) {
             System.out.println("Player B wins");
             wins = true;
-        }
+          }
         
         return wins;
     }
     
-	public static void main (String[] args) throws java.lang.Exception
-	{
+	public static void main (String[] args) throws java.lang.Exception {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the string of scores.\n");
 		String scoreString = scanner.nextLine();
 		
 		TennisGame tennisGame = new TennisGame();
-		
 		tennisGame.computeScores(scoreString);
-		
 		return;
 	}
 }
