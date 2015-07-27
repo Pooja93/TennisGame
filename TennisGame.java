e413cc1d2aaa62f2af2051112adfad076e9ef155
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-27 11:01:45 +0530

    constructor added

--- TennisGame.java
+++ TennisGame.java
@@ -4,6 +4,14 @@ import java.io.*;
 
 class TennisGame
 {
+	int playerA;
+	int playerB;
+	
+	TennisGame()
+	{
+		playerA = 0;
+		playerB = 0;
+	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner scanner = new Scanner(System.in);
