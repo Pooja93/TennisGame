2f3da8978a12356e30495674e1da82ba376624a1
unknown <test@L-MAA-25004240.paypalcorp.com>
2015-07-27 10:44:45 +0530

    Initial commit

---
+++ TennisGame.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class TennisGame
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the string of scores.\n");
+		String scoreString = scanner.nextLine();
+		
+		TennisGame tennisGame = new TennisGame();
+		
+		tennisGame.computeScores(scoreString);
+		
+		return;
+	}
+}
